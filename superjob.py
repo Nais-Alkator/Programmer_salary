ceeac65dfd20db1d76fed59d0611170cfce43661
Alkator-Nori <nais000k@gmail.com>
2019-08-17 23:44:05 +0300

    Fix typos, syntaxis

--- superjob.py
+++ superjob.py
@@ -7,112 +7,151 @@ load_dotenv()
 SUPERJOB_TOKEN = os.getenv("SUPERJOB_TOKEN")
 URL = "https://api.superjob.ru/2.0/vacancies/"
 HEADERS = {"X-Api-App-Id": SUPERJOB_TOKEN}
+TOWN_ID = 4
+CATALOG_OF_PROFESSION = 48
+
 
 def get_developers_professions():
-  params = {"town": 4, "catalogues": 48}
-  response = requests.get(URL, headers=HEADERS, params=params)
-  response = response.json()
-  data = response["objects"]
-  professions = [item["profession"] for item in data]
-  return professions
+    params = {"town": TOWN_ID, "catalogues": CATALOG_OF_PROFESSION}
+    response = requests.get(URL, headers=HEADERS, params=params)
+    response = response.json()
+    data = response["objects"]
+    professions = [item["profession"] for item in data]
+    return professions
 
-def get_predict_rub_salary_for_Superjob(vacancy):
-	params = {"town": 4, "catalogues": 48, "keyword": "Разработчик {}".format(vacancy)}
-	response = requests.get(URL, headers=HEADERS, params=params)
-	response = response.json()
-	data = response["objects"]
-	predict_rub_salary = list()
-	for salary in data:
-		if salary["currency"] == "rub":
-			if salary["payment_from"] == 0 and salary["payment_to"] == 0:
-				average_salary = None
-			elif salary["payment_from"] != 0 and salary["payment_to"] == 0:
-				average_salary = salary["payment_from"] * 0.8
-			elif salary["payment_from"] == 0 and salary["payment_to"] != 0:
-				average_salary = salary["payment_to"] * 0.8
-			elif salary["payment_from"] != 0 and salary["payment_to"] != 0:
-				average_salary = (salary["payment_from"] + salary["payment_to"]) / 2
-			predict_rub_salary.append(average_salary)
-	return predict_rub_salary
+
+def get_predict_rub_salary_for_superjob(vacancy):
+    params = {"town": TOWN_ID, "catalogues": CATALOG_OF_PROFESSION,
+              "keyword": "Разработчик {}".format(vacancy)}
+    response = requests.get(URL, headers=HEADERS, params=params)
+    if not response:
+        raise ApiResponseFormatError("Ошибка")
+    response = response.json()
+    data = response["objects"]
+    predict_rub_salary = list()
+    for salary in data:
+        if salary["currency"] == "rub":
+            if salary["payment_from"] == 0 and salary["payment_to"] == 0:
+                average_salary = None
+            elif salary["payment_from"] != 0 and salary["payment_to"] == 0:
+                average_salary = salary["payment_from"] * 0.8
+            elif salary["payment_from"] == 0 and salary["payment_to"] != 0:
+                average_salary = salary["payment_to"] * 0.8
+            elif salary["payment_from"] != 0 and salary["payment_to"] != 0:
+                average_salary = (
+                    salary["payment_from"] + salary["payment_to"]) / 2
+            predict_rub_salary.append(average_salary)
+    return predict_rub_salary
+
 
 def get_global_data(vacancy):
-  global_data = list()
-  page = 0
-  pages_number = 1
-  while page < pages_number:
-  	params = {"keyword":"{}".format(vacancy), "town": 4, "catalogues": 48}
-  	page_data = requests.get(URL, params=params, headers=HEADERS)
-  	page_data = page_data.json()
-  	pages_number = page_data['total']
-  	print("{} Добавлено из {}".format(page, page_data["total"]))
-  	page += 1
-  	global_data.append(page_data["objects"])
-  return global_data
+    global_data = list()
+    page = 0
+    pages_number = 1
+    while page < pages_number:
+        params = {
+            "keyword": "{}".format(vacancy),
+            "town": TOWN_ID,
+            "catalogues": CATALOG_OF_PROFESSION}
+        page_data = requests.get(URL, params=params, headers=HEADERS)
+        if not page_data:
+            raise ApiResponseFormatError("Ошибка")
+        page_data = page_data.json()
+        pages_number = page_data['total']
+        print("{} Добавлено из {}".format(page, page_data["total"]))
+        page += 1
+        global_data.append(page_data["objects"])
+    return global_data
+
 
 def get_quantity_of_vacancies(vacancy):
-  params = {"keyword":"Разработчик {}".format(vacancy), "town": 4, "catalogues": 48}
-  response = requests.get(URL, headers=HEADERS, params=params)
-  response = response.json()
-  quantity_of_vacancies = response["total"]
-  return quantity_of_vacancies
+    params = {"keyword": "Разработчик {}".format(
+        vacancy), "town": TOWN_ID, "catalogues": CATALOG_OF_PROFESSION}
+    response = requests.get(URL, headers=HEADERS, params=params)
+    if not response:
+        raise ApiResponseFormatError("Ошибка")
+    response = response.json()
+    quantity_of_vacancies = response["total"]
+    return quantity_of_vacancies
+
 
 def get_vacancy_processed(vacancy):
-  predict_salary = get_predict_rub_salary_for_Superjob(vacancy)
-  vacancy_prosecced = 0
-  for salary in predict_salary:
-  	if salary != None:
-  		vacancy_prosecced += 1
-  return vacancy_prosecced
+    predict_salary = get_predict_rub_salary_for_superjob(vacancy)
+    vacancy_prosecced = 0
+    for salary in predict_salary:
+        if salary is not None:
+            vacancy_prosecced += 1
+    return vacancy_prosecced
+
 
 def get_average_salary_for_one_vacancy(vacancy):
-  predict_salary = get_predict_rub_salary_for_Superjob(vacancy)
-  average_salaries_for_count = list()
-  for salary in predict_salary:
-  	if salary != None:
-  		salary = int(salary)
-  		average_salaries_for_count.append(salary)
-  if len(average_salaries_for_count) != 0:
-  	final_average_salary = sum(average_salaries_for_count) / len(average_salaries_for_count)
-  else:
-  	final_average_salary = 0
-  return int(final_average_salary)
+    predict_salary = get_predict_rub_salary_for_superjob(vacancy)
+    average_salaries_for_count = list()
+    for salary in predict_salary:
+        if salary is not None:
+            salary = int(salary)
+            average_salaries_for_count.append(salary)
+    if len(average_salaries_for_count) != 0:
+        final_average_salary = sum(
+            average_salaries_for_count) / len(average_salaries_for_count)
+    else:
+        final_average_salary = 0
+    return int(final_average_salary)
 
-def get_statistics_for_languages_from_Superjob():
-  top_10_programming_languages = ["Javascript", "Java", "Python", "Ruby", "PHP", "C++", "C#", "C", "Go", "Scala"]
-  statistics_for_all_languages = {}
-  for language in top_10_programming_languages:
-	  statistics_for_one_language = {
-		  language: {
-		  "vacancy_found": get_quantity_of_vacancies(language),
-		  "vacancy_processed": get_vacancy_processed(language),
-		  "average_salary": get_average_salary_for_one_vacancy(language)
-		  }
-	  }
-	  statistics_for_all_languages.update(statistics_for_one_language)
-  return(statistics_for_all_languages)
+
+def get_statistics_for_languages_from_superjob():
+    top_10_programming_languages = [
+        "Javascript",
+        "Java",
+        "Python",
+        "Ruby",
+        "PHP",
+        "C++",
+        "C#",
+        "C",
+        "Go",
+        "Scala"]
+    statistics_for_all_languages = {}
+    for language in top_10_programming_languages:
+        statistics_for_one_language = {
+            language: {
+                "vacancy_found": get_quantity_of_vacancies(language),
+                "vacancy_processed": get_vacancy_processed(language),
+                "average_salary": get_average_salary_for_one_vacancy(language)
+            }
+        }
+        statistics_for_all_languages.update(statistics_for_one_language)
+    return(statistics_for_all_languages)
+
 
 def print_statistics_table_view():
-  data = get_statistics_for_languages_from_Superjob()
-  TABLE_DATA = [["Язык", "Вакансий найдено", "Вакансий обработано", "Средняя зарплата"]]
-  languages = data.keys()
-  for language in languages:
-  	row = []
-  	row.append(language)
-  	keys_data = data[language]
-  	keys_data = keys_data.values()
-  	for key in keys_data:
-  		row.append(key)
-  	TABLE_DATA.append(row)
-  title = 'SuperJob Moscow'
-  table = AsciiTable(TABLE_DATA, title)
-  print(table.table)
-  print()
+    stat_for_languages = get_statistics_for_languages_from_superjob()
+    table_data = [["Язык",
+                   "Вакансий найдено",
+                   "Вакансий обработано",
+                   "Средняя зарплата"]]
+    for language in stat_for_languages:
+        row = [language]
+        language_keys = stat_for_languages[language]
+        language_values = language_keys.values()
+        row.extend(language_values)
+        table_data.append(row)
+    title = 'SuperJob Moscow'
+    table = AsciiTable(table_data, title)
+    print(table.table)
+    print()
+
+
+class ApiResponseFormatError(KeyError):
+    pass
+
 
 if __name__ == "__main__":
-  try: print_statistics_table_view()
-  except requests.exceptions.ConnectionError as error:
-    exit("Can't get data from server:\n{0}".format(error))
-  except requests.exceptions.HTTPError as error:
-    exit("Can't get data from server:\n{0}".format(error))
-  except KeyError as error:
-    exit("Неверный URL")
+    try:
+        print_statistics_table_view()
+    except requests.exceptions.ConnectionError as error:
+        exit("Can't get data from server:\n{0}".format(error))
+    except requests.exceptions.HTTPError as error:
+        exit("Can't get data from server:\n{0}".format(error))
+    except ApiResponseFormatError as error:
+        exit("Ошибка программы")
--- test.py
+++ test.py
@@ -1,84 +1,154 @@
+import requests
+from terminaltables import AsciiTable, DoubleTable, SingleTable
 
-from colorclass import Color, Windows
-
-from terminaltables import SingleTable
-
-
-def table_server_timings():
-    """Return table string to be printed."""
-    table_data = [
-        [Color('{autogreen}<10ms{/autogreen}'), '192.168.0.100, 192.168.0.101'],
-        [Color('{autoyellow}10ms <= 100ms{/autoyellow}'), '192.168.0.102, 192.168.0.103'],
-        [Color('{autored}>100ms{/autored}'), '192.168.0.105'],
-    ]
-    table_instance = SingleTable(table_data)
-    table_instance.inner_heading_row_border = False
-    return table_instance.table
+URL = "https://api.hh.ru/vacancies/"
+HEADERS = {"User-Agent": "User-Agent"}
+SEARCH_AREA = 1
+SEARCH_PERIOD = 30
 
 
-def table_server_status():
-    """Return table string to be printed."""
-    table_data = [
-        [Color('Low Space'), Color('{autocyan}Nominal Space{/autocyan}'), Color('Excessive Space')],
-        [Color('Low Load'), Color('Nominal Load'), Color('{autored}High Load{/autored}')],
-        [Color('{autocyan}Low Free RAM{/autocyan}'), Color('Nominal Free RAM'), Color('High Free RAM')],
-    ]
-    table_instance = SingleTable(table_data, '192.168.0.105')
-    table_instance.inner_heading_row_border = False
-    table_instance.inner_row_border = True
-    table_instance.justify_columns = {0: 'center', 1: 'center', 2: 'center'}
-    return table_instance.table
+def get_quantity_of_vacancies(vacancy):
+    params = {
+        "text": "Разработчик {}".format(vacancy),
+        "area": SEARCH_AREA,
+        "period": SEARCH_PERIOD}
+    response = requests.get(URL, params=params, headers=HEADERS)
+    if not response:
+        raise ApiResponseFormatError("Ошибка")
+    response.ok
+    response = response.json()
+    quantity_of_vacancy = response["found"]
+    return quantity_of_vacancy
 
 
-def table_abcd():
-    """Return table string to be printed. Two tables on one line."""
-    table_instance = SingleTable([['A', 'B'], ['C', 'D']])
-
-    # Get first table lines.
-    table_instance.outer_border = False
-    table_inner_borders = table_instance.table.splitlines()
-
-    # Get second table lines.
-    table_instance.outer_border = True
-    table_instance.inner_heading_row_border = False
-    table_instance.inner_column_border = False
-    table_outer_borders = table_instance.table.splitlines()
-
-    # Combine.
-    smallest, largest = sorted([table_inner_borders, table_outer_borders], key=len)
-    smallest += [''] * (len(largest) - len(smallest))  # Make both same size.
-    combined = list()
-    for i, row in enumerate(largest):
-        combined.append(row.ljust(10) + '          ' + smallest[i])
-    return '\n'.join(combined)
+def get_salaries_of_developers(vacancy):
+    global_data = get_global_vacancy_data(vacancy)
+    salaries_in_rub = []
+    salaries = []
+    for page in global_data:
+        for item in page:
+            item = item["salary"]
+            salaries.append(item)
+    for salary in salaries:
+        if salary["currency"] != "RUR":
+            salary = {"from": None, "to": None}
+        else:
+            salary = salary
+        salaries_in_rub.append(salary)
+    return salaries_in_rub
 
 
-def main():
-    """Main function."""
-    Windows.enable(auto_colors=True, reset_atexit=True)  # Does nothing if not on Windows.
+def get_predict_rub_salary(vacancy):
+    salaries = get_salaries_of_developers(vacancy)
+    predict_salary = []
+    for salary in salaries:
+        salary_from = salary["from"]
+        salary_to = salary["to"]
+        if salary_from is None and salary_to is None:
+            average_salary = 0
+        elif salary_from is not None and salary_to is None:
+            average_salary = salary_from * 1.2
+            average_salary = int(average_salary)
+        elif salary_from is None and (salary_to) is not None:
+            average_salary = salary_to * 0.8
+            average_salary = int(average_salary)
+        elif salary_from is not None and salary_to is not None:
+            average_salary = (salary_from + salary_to) / 2
+            average_salary = int(average_salary)
+        predict_salary.append(average_salary)
+    return predict_salary
 
-    # Server timings.
-    print(table_server_timings())
+
+def get_vacancy_processed(vacancy):
+    predict_salary = get_predict_rub_salary(vacancy)
+    vacancy_prosecced = 0
+    for salary in predict_salary:
+        if salary != 0:
+            vacancy_prosecced += 1
+    return vacancy_prosecced
+
+
+def get_average_salary_for_one_vacancy(vacancy):
+    average_salary_for_one_vacancy = sum(
+        get_predict_rub_salary(vacancy)) / get_vacancy_processed(vacancy)
+    return int(average_salary_for_one_vacancy)
+
+
+def get_statistics_for_languages_from_HeadHunter():
+    top_10_programming_languages = [
+        "Javascript",
+        "Java",
+        "Python",
+        "Ruby",
+        "PHP",
+        "C++",
+        "C#",
+        "C",
+        "Go",
+        "Scala"]
+    statistics_for_all_languages = dict()
+    for language in top_10_programming_languages:
+        statistics_for_one_language = {
+            language: {
+                "vacancy_found": get_quantity_of_vacancies(language),
+                "vacancy_processed": get_vacancy_processed(language),
+                "average_salary": get_average_salary_for_one_vacancy(language)
+            }
+        }
+        statistics_for_all_languages.update(statistics_for_one_language)
+    return statistics_for_all_languages
+
+
+def get_global_vacancy_data(vacancy):
+    global_data = list()
+    page = 0
+    pages_number = 1
+    while page < pages_number:
+        params = {
+            "text": "{}".format(vacancy),
+            "area": SEARCH_AREA,
+            "period": SEARCH_PERIOD,
+            "only_with_salary": "True",
+            "page": page}
+        page_data = requests.get(URL, params=params, headers=HEADERS)
+        if not page_data:
+            raise ApiResponseFormatError("Ошибка")
+        page_data = page_data.json()
+        pages_number = page_data['pages']
+        print("{} Добавлено из {}".format(page, page_data["pages"]))
+        page += 1
+        global_data.append(page_data["items"])
+    return global_data
+
+
+def print_statistics_table_view():
+    stat_for_languages = get_statistics_for_languages_from_HeadHunter()
+    table_data = [["Язык",
+                   "Вакансий найдено",
+                   "Вакансий обработано",
+                   "Средняя зарплата"]]
+    for language in stat_for_languages:
+        row = [language]
+        language_keys = stat_for_languages[language]
+        language_values = language_keys.values()
+        row.extend(language_values)
+        table_data.append(row)
+    title = 'HeadHunter Moscow'
+    table = AsciiTable(table_data, title)
+    print(table.table)
     print()
 
-    # Server status.
-    print(table_server_status())
-    print()
 
-    # Two A B C D tables.
-    print(table_abcd())
-    print()
-
-    # Instructions.
-    table_instance = SingleTable([['Obey Obey Obey Obey']], 'Instructions')
-    print(table_instance.table)
-    print()
+class ApiResponseFormatError(KeyError):
+    pass
 
 
-if __name__ == '__main__':
-    main()
-
-
-
-
-
+if __name__ == "__main__":
+    try:
+        print_statistics_table_view()
+    except requests.exceptions.ConnectionError as error:
+        exit("Can't get data from server:\n{0}".format(error))
+    except requests.exceptions.HTTPError as error:
+        exit("Can't get data from server:\n{0}".format(error))
+    except ApiResponseFormatError as error:
+        exit("Ошибка программы")
